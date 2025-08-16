0e49d84a48084d950ab00c775d977a2eabdb5743
Priyansh Saklecha <saklecha.priyansh817@gmail.com>
2025-08-16 22:45:30 +0530

    server.js changes

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,6 @@ import connectCloudinary from "./config/cloudinary.js"
 import userRouter from "./routes/userRoute.js"
 import expertRouter from "./routes/expertRoute.js"
 import adminRouter from "./routes/adminRoute.js"
-import path from "path";
-import { fileURLToPath } from "url";
 
 // app config
 const app = express()
@@ -15,12 +13,8 @@ const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-console.log(__dirname);
 // middlewares
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "/dist")));
 app.use(cors())
 
 // api endpoints
@@ -28,13 +22,8 @@ app.use("/api/user", userRouter)
 app.use("/api/admin", adminRouter)
 app.use("/api/expert", expertRouter)
 
-// app.get("/", (req, res) => {
-//   res.send("API Working")
-// });
-// /admin/*
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/dist/index.html"));
+app.get("/", (req, res) => {
+  res.send("API Working")
 });
-
 
 app.listen(port, () => console.log(`Server started on PORT:${port}`))
